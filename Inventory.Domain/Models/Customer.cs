e5b016a3f3f8b9048b6c57c9bdb331476f653fc3
Alejandra Adames <aadames@marzanconsulting.com>
2022-05-20 03:12:38 -0400

    Add primary key property

--- Inventory.Domain/Models/Customer.cs
+++ Inventory.Domain/Models/Customer.cs
@@ -1,8 +1,12 @@
-﻿namespace Inventory.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Domain.Models
 {
     public class Customer
     {
         // Define Properties
+        [Key]
+        public int Id { get; set; }
         public string? CustomerName { get; set; }
     }
 }
--- Inventory.Domain/Models/Product.cs
+++ Inventory.Domain/Models/Product.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Inventory.Domain.Models
 {
     public class Product
     {
+        // Define Properties
+        [Key]
+        public int Id { get; set; }
         public string? ProductName { get; set; }
     }
 }
