7b9f1fb8925774cff61c57fd54a631e5b8a663c2
Alejandra Adames <aadames@marzanconsulting.com>
2022-05-20 20:19:46 -0400

    feat: define methods to add and get customers in the interface

--- Inventory.Domain/Abstractions/ICustomerService.cs
+++ Inventory.Domain/Abstractions/ICustomerService.cs
@@ -1,7 +1,12 @@
-﻿namespace Inventory.Domain.Abstractions
+﻿using Inventory.Domain.Models;
+
+namespace Inventory.Domain.Abstractions
 {
     public interface ICustomerService
     {
         // Define methods to the Customer services
+
+        Task<List<Customer>> GetCustomersAsync();
+        Task<bool> AddCustomer(Customer customer);
     }
 }
