7812aefd5d32ac5117703929c06ac0b3d2daf894
Alejandra Adames <aadames@marzanconsulting.com>
2022-05-29 15:32:29 -0400

    Add domain folder in models project to include domain classes

---
+++ Inventory.Domain/Domain/Category.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Models.Abstractions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inventory.Models.Domain
+{
+    public class Category: BaseEntity
+    {
+        [Required]
+        [StringLength(12)]
+        [Column("Category Namr")]
+        public string CategoryName { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Inventory.Domain/Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Abstractions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models.Domain
+{
+    public class User:BasicInformation
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool Role { get; set; }
+    }
+}
