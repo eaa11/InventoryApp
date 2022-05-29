a7c618c4521ea98981b8fe3c24e8dc05cefe74be
Alejandra Adames <aadames@marzanconsulting.com>
2022-05-29 15:35:18 -0400

    Feat: add BaseEntity abstract class to reuse properties within classes

---
+++ Inventory.Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inventory.Models.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        [NotMapped]
+        public State State { get; set; }
+
+        [Required]
+        public DateOnly RegisterDate { get; set; }
+
+        public BaseEntity() => State = State.Unchanged;
+    }
+}
